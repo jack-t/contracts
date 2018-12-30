5c4de2e9d8966eeefcd447d86a896600a2f2bf7a
jack-t <jackmaverick1@gmail.com>
2018-12-30 00:25:07 -0500

    Start testing lexing

--- src/lex/mod.rs
+++ src/lex/mod.rs
@@ -1,5 +1,7 @@
 use regex::Regex;
+use std::str::Chars;
 
+#[derive(Clone)]
 pub enum Token {
 	KeyType,
 	KeyFn,
@@ -40,7 +42,7 @@ pub enum Token {
 	SymRightBracket,
 }
 
-type TokenProducer = Fn(&mut str) -> Option<(Token, usize)>;
+type TokenProducer = Fn(&str) -> Option<(Token, usize)>;
 
 pub struct Lexer {
 	producers: Vec<Box<TokenProducer>>
@@ -49,21 +51,72 @@ pub struct Lexer {
 impl Lexer {
 	pub fn new() -> Lexer {
 		Lexer {
-			producers: vec![lex_literal("type", Token::KeyType)],
+			producers: vec![
+				lex_literal("type", Token::KeyType),
+				lex_literal("fn", Token::KeyFn),
+				lex_literal("float", Token::KeyFloat),
+				lex_literal("int", Token::KeyInt),
+				lex_literal("char", Token::KeyChar),
+				lex_literal("array", Token::KeyArray),
+				lex_literal("struct", Token::KeyStruct),
+				lex_literal("contract", Token::KeyContract),
+				lex_literal("return", Token::KeyReturn),
+				lex_literal("while", Token::KeyWhile),
+				lex_literal("if", Token::KeyIf),
+			],
 		}
+	}
+
+	pub fn lex(&self, code: &mut str) -> Vec<Token> {
+		let mut ret = Vec::new();
+		let mut chars = code.chars();
+		while code.len() > 0 {
+			if let Some(tok) = self.tokenize(&mut chars) {
+				ret.push(tok);
+			} else {
+				panic!("Error during tokenization, code starting here: {}", code);
+			}
+		}
+		ret
+	}
+
+	fn tokenize(&self, code: &mut Chars) -> Option<Token> {
+		for &prod in self.producers {
+			if let Some((tok, offset)) = prod(code.as_str()) {
+				code.skip(offset);
+				return Some(tok);
+			}
+		}
+		None
 	}
 }
 
-fn lex_literal(lit: &str, tok: Token) -> Box<TokenProducer> {
-	Box::new(move |code: &mut str| {
-		let regex = Regex::new((("^\s*(?P<cap>".to_string() + lit).to_string() + ">.*").as_str()).unwrap();
+fn lex_literal(lit: &'static str, tok: Token) -> Box<TokenProducer> {
+	Box::new(move |code: &str| {
+		let regex = Regex::new(((r"^\s*(?P<cap>".to_string() + lit).to_string() + ">.*").as_str()).unwrap();
 		if let Some(captures) = regex.captures(code) {
 			match captures.name("cap") {
-				Some(cap) => Some((tok, cap.as_str().len())),
+				Some(cap) => Some((tok.clone(), cap.as_str().len())),
 				_ => unreachable!()
 			}
 		} else {
 			None
 		}
 	})
+}
+
+#[cfg(test)]
+mod tests {
+
+	mod keywords {
+		#[test]
+		fn it_gets_key_type() {
+
+			let code = " type  xyz";
+
+			
+
+		}
+	}
+
 }
