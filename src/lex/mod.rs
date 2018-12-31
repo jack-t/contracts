267e0447c9be2262eac1a72e31e9eebe0395348d
jack-t <jackmaverick1@gmail.com>
2018-12-31 00:05:28 -0500

    Get rid of the silly prefixes on symbol names

--- src/lex/mod.rs
+++ src/lex/mod.rs
@@ -3,41 +3,41 @@ use lazy_static::*;
 
 #[derive(PartialEq, Clone, Debug)]
 pub enum Token {
-	KeyType,
-	KeyFn,
-	KeyFloat,
-	KeyInt,
-	KeyChar,
-	KeyArray,
-	KeyStruct,
-	KeyContract,
-	KeyReturn,
-	KeyWhile,
-	KeyIf,
+	Type,
+	Fn,
+	Float,
+	Int,
+	Char,
+	Array,
+	Struct,
+	Contract,
+	Return,
+	While,
+	If,
 	Id(String),
 	FloatLiteral(f64),
 	IntLiteral(u64),
 	StringLiteral(String),
-	Char(char),
-	OpPlus,
-	OpMinus,
-	OpStar,
-	OpSlash,
-	OpPercent,
-	OpEquality,
-	OpAssign,
-	OpInequality,
-	OpLess,
-	OpLessOrEqual,
-	OpGreater,
-	OpGreaterOrEqual,
-	OpLeftParen,
-	OpRightParen,
-	SymSemicolon,
-	SymComma,
-	SymPipe,
-	SymLeftBracket,
-	SymRightBracket,
+	CharLiteral(char),
+	Plus,
+	Minus,
+	Star,
+	Slash,
+	Percent,
+	Equality,
+	Assign,
+	Inequality,
+	Less,
+	LessOrEqual,
+	Greater,
+	GreaterOrEqual,
+	LeftParen,
+	RightParen,
+	Semicolon,
+	Comma,
+	Pipe,
+	LeftBracket,
+	RightBracket,
 }
 
 type TokenProducer = Fn(&str) -> Option<(Token, usize)>;
@@ -50,41 +50,41 @@ impl Lexer {
 	pub fn new() -> Lexer {
 		Lexer {
 			producers: vec![
-				lex_literal("type", Token::KeyType),
-				lex_literal("fn", Token::KeyFn),
-				lex_literal("float", Token::KeyFloat),
-				lex_literal("int", Token::KeyInt),
-				lex_literal("char", Token::KeyChar),
-				lex_literal("array", Token::KeyArray),
-				lex_literal("struct", Token::KeyStruct),
-				lex_literal("contract", Token::KeyContract),
-				lex_literal("return", Token::KeyReturn),
-				lex_literal("while", Token::KeyWhile),
-				lex_literal("if", Token::KeyIf),
+				lex_exactly("type", Token::Type),
+				lex_exactly("fn", Token::Fn),
+				lex_exactly("float", Token::Float),
+				lex_exactly("int", Token::Int),
+				lex_exactly("char", Token::Char),
+				lex_exactly("array", Token::Array),
+				lex_exactly("struct", Token::Struct),
+				lex_exactly("contract", Token::Contract),
+				lex_exactly("return", Token::Return),
+				lex_exactly("while", Token::While),
+				lex_exactly("if", Token::If),
 				Box::new(lex_id),
 				Box::new(lex_float_lit), // this comes before int_lit because the float regex is greedy
 				Box::new(lex_int_lit),
 				Box::new(lex_string_lit),
 				Box::new(lex_char),
-				lex_literal("\\+", Token::OpPlus),
-				lex_literal("\\-", Token::OpMinus),
-				lex_literal("\\*", Token::OpStar),
-				lex_literal("/", Token::OpSlash),
-				lex_literal("%", Token::OpPercent),
-				lex_literal("==", Token::OpEquality),
-				lex_literal("=", Token::OpAssign),
-				lex_literal("!=", Token::OpInequality),
-				lex_literal("<=", Token::OpLessOrEqual), // order matters with these tokens
-				lex_literal("<", Token::OpLess),
-				lex_literal(">=", Token::OpGreaterOrEqual),
-				lex_literal(">", Token::OpGreater),
-				lex_literal("\\(", Token::OpLeftParen),
-				lex_literal("\\)", Token::OpRightParen),
-				lex_literal(";", Token::SymSemicolon),
-				lex_literal(",", Token::SymComma),
-				lex_literal("\\|", Token::SymPipe),
-				lex_literal("\\]", Token::SymRightBracket),
-				lex_literal("\\[", Token::SymLeftBracket),
+				lex_exactly("\\+", Token::Plus),
+				lex_exactly("\\-", Token::Minus),
+				lex_exactly("\\*", Token::Star),
+				lex_exactly("/", Token::Slash),
+				lex_exactly("%", Token::Percent),
+				lex_exactly("==", Token::Equality),
+				lex_exactly("=", Token::Assign),
+				lex_exactly("!=", Token::Inequality),
+				lex_exactly("<=", Token::LessOrEqual), // order matters with these tokens
+				lex_exactly("<", Token::Less),
+				lex_exactly(">=", Token::GreaterOrEqual),
+				lex_exactly(">", Token::Greater),
+				lex_exactly("\\(", Token::LeftParen),
+				lex_exactly("\\)", Token::RightParen),
+				lex_exactly(";", Token::Semicolon),
+				lex_exactly(",", Token::Comma),
+				lex_exactly("\\|", Token::Pipe),
+				lex_exactly("\\]", Token::RightBracket),
+				lex_exactly("\\[", Token::LeftBracket),
 
 			],
 		}
@@ -114,7 +114,7 @@ impl Lexer {
 	}
 }
 
-fn lex_literal(lit: &'static str, tok: Token) -> Box<TokenProducer> {
+fn lex_exactly(lit: &'static str, tok: Token) -> Box<TokenProducer> {
 	Box::new(move |code: &str| {
 		let regex = Regex::new(((r"^\s*(?P<cap>".to_string() + lit).to_string() + ").*").as_str()).unwrap();
 		if let Some(caps) = regex.captures(code) {
@@ -200,7 +200,7 @@ fn lex_char(code: &str) -> Option<(Token, usize)> {
 		match caps.name("cap") {
 			Some(mat) => {
 					let ch = mat.as_str().chars().next().unwrap();
-					Some((Token::Char(ch), mat.end() + 1))
+					Some((Token::CharLiteral(ch), mat.end() + 1))
 				}, // + 1 to account for the end quote
 			_ => unreachable!() // because it captured something and there's only one capture group
 		}
@@ -211,240 +211,255 @@ fn lex_char(code: &str) -> Option<(Token, usize)> {
 
 #[cfg(test)]
 mod tests {
-	mod keywords {
-		use super::super::*;
-		#[test]
-		fn it_gets_key_type() {
-			
-			let code = " type ";
+	use super::*;
+	#[test]
+	fn it_gets_key_type() {
+		
+		let code = " type ";
+		let lexer = Lexer::new();
+		let results = lexer.lex(code);
+
+		assert_eq!(results, vec![Token::Type]);
+
+	}
+	#[test]
+	fn it_gets_id() {
+		
+		let code = " type _x1yz ";
+		let lexer = Lexer::new();
+		let results = lexer.lex(code);
+
+		assert_eq!(results, vec![Token::Type, Token::Id("_x1yz".to_string())]);
+
+	}
+	#[test]
+	#[should_panic]
+	fn it_rejects_ids_with_initial_numeral() {
+		
+		let code = " _1 ";
+		let lexer = Lexer::new();
+		lexer.lex(code);
+	}
+
+	#[test]
+	#[should_panic]
+	fn it_rejects_ids_with_initial_numeral_no_underscore() {
+		
+		let code = " 1a ";
+		let lexer = Lexer::new();
+		lexer.lex(code);
+	}
+
+
+	#[test]
+	fn it_gets_short_id() {
+		
+		let code = " a ";
+		let lexer = Lexer::new();
+		let results = lexer.lex(code);
+
+		assert_eq!(results, vec![Token::Id("a".to_string())]);
+
+	}
+
+	#[test]
+	fn it_gets_int_ignores_octal() {
+		
+		let code = " 010 ";
+		let lexer = Lexer::new();
+		let results = lexer.lex(code);
+
+		assert_eq!(results, vec![Token::IntLiteral(10)]);
+
+	}
+
+	#[test]
+	#[should_panic]
+	fn it_rejects_ints_with_letters() {
+		let code = " 1b ";
+		let lexer = Lexer::new();
+		lexer.lex(code);
+	}
+
+	#[test]
+	fn it_gets_packed_arith() {
+
+		let code = " 1+2 ";
+		let lexer = Lexer::new();
+		let results = lexer.lex(code);
+
+		assert_eq!(results, vec![Token::IntLiteral(1), Token::Plus, Token::IntLiteral(2)]);
+
+	}
+
+	#[test]
+	fn it_gets_float() {
+		
+		let code = " 1.25 ";
+		let lexer = Lexer::new();
+		let results = lexer.lex(code);
+
+		assert_eq!(results, vec![Token::FloatLiteral(1.25)]);
+
+	}
+
+
+	#[test]
+	fn it_gets_strings_with_quotes() {
+		
+		let code = r#" "a\"'b\"c" def "#;
+		let lexer = Lexer::new();
+		let results = lexer.lex(code);
+
+		assert_eq!(results, vec![Token::StringLiteral("a\"'b\"c".to_string()), Token::Id("def".to_string())]);
+
+	}
+
+	#[test]
+	fn it_gets_chars() {
+		
+		let code = r#" '	' a 'b' "#; // it even gets raw tabs!
+		let lexer = Lexer::new();
+		let results = lexer.lex(code);
+
+		assert_eq!(results, vec![Token::CharLiteral('\t'), Token::Id("a".to_string()), Token::CharLiteral('b')]);
+
+	}
+
+	// I didn't want to clutter the list of tests with things that (I think?) will rarely go wrong.
+	#[test]
+	fn it_gets_ops() {
+		{
+			let code = r#" + "#;
 			let lexer = Lexer::new();
 			let results = lexer.lex(code);
-
-			assert_eq!(results, vec![Token::KeyType]);
-
+			assert_eq!(results, vec![Token::Plus]);
 		}
-		#[test]
-		fn it_gets_id() {
-			
-			let code = " type _x1yz ";
+		{
+			let code = r#" - "#;
 			let lexer = Lexer::new();
 			let results = lexer.lex(code);
-
-			assert_eq!(results, vec![Token::KeyType, Token::Id("_x1yz".to_string())]);
-
+			assert_eq!(results, vec![Token::Minus]);
 		}
-		#[test]
-		#[should_panic]
-		fn it_rejects_ids_with_initial_numeral() {
-			
-			let code = " _1 ";
-			let lexer = Lexer::new();
-			lexer.lex(code);
-		}
-
-		#[test]
-		#[should_panic]
-		fn it_rejects_ids_with_initial_numeral_no_underscore() {
-			
-			let code = " 1a ";
-			let lexer = Lexer::new();
-			lexer.lex(code);
-		}
-
-
-		#[test]
-		fn it_gets_short_id() {
-			
-			let code = " a ";
+		{
+			let code = r#" * "#;
 			let lexer = Lexer::new();
 			let results = lexer.lex(code);
-
-			assert_eq!(results, vec![Token::Id("a".to_string())]);
-
+			assert_eq!(results, vec![Token::Star]);
 		}
-
-		#[test]
-		fn it_gets_int_ignores_octal() {
-			
-			let code = " 010 ";
+		{
+			let code = r#" / "#;
 			let lexer = Lexer::new();
 			let results = lexer.lex(code);
-
-			assert_eq!(results, vec![Token::IntLiteral(10)]);
-
+			assert_eq!(results, vec![Token::Slash]);
 		}
-
-		#[test]
-		#[should_panic]
-		fn it_rejects_ints_with_letters() {
-			let code = " 1b ";
-			let lexer = Lexer::new();
-			lexer.lex(code);
-		}
-
-		#[test]
-		fn it_gets_packed_arith() {
-
-			let code = " 1+2 ";
+		{
+			let code = r#" % "#;
 			let lexer = Lexer::new();
 			let results = lexer.lex(code);
-
-			assert_eq!(results, vec![Token::IntLiteral(1), Token::OpPlus, Token::IntLiteral(2)]);
-
+			assert_eq!(results, vec![Token::Percent]);
 		}
-
-		#[test]
-		fn it_gets_float() {
-			
-			let code = " 1.25 ";
+		{
+			let code = r#" == "#;
 			let lexer = Lexer::new();
 			let results = lexer.lex(code);
-
-			assert_eq!(results, vec![Token::FloatLiteral(1.25)]);
-
+			assert_eq!(results, vec![Token::Equality]);
 		}
-
-
-		#[test]
-		fn it_gets_strings_with_quotes() {
-			
-			let code = r#" "a\"'b\"c" def "#;
+		{
+			let code = r#" = "#;
 			let lexer = Lexer::new();
 			let results = lexer.lex(code);
-
-			assert_eq!(results, vec![Token::StringLiteral("a\"'b\"c".to_string()), Token::Id("def".to_string())]);
-
+			assert_eq!(results, vec![Token::Assign]);
 		}
-
-		#[test]
-		fn it_gets_chars() {
-			
-			let code = r#" '	' a 'b' "#; // it even gets raw tabs!
+		{
+			let code = r#" != "#;
 			let lexer = Lexer::new();
 			let results = lexer.lex(code);
-
-			assert_eq!(results, vec![Token::Char('\t'), Token::Id("a".to_string()), Token::Char('b')]);
-
+			assert_eq!(results, vec![Token::Inequality]);
 		}
-
-		// I didn't want to clutter the list of tests with things that (I think?) will rarely go wrong.
-		#[test]
-		fn it_gets_ops() {
-			{
-				let code = r#" + "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpPlus]);
-			}
-			{
-				let code = r#" - "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpMinus]);
-			}
-			{
-				let code = r#" * "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpStar]);
-			}
-			{
-				let code = r#" / "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpSlash]);
-			}
-			{
-				let code = r#" % "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpPercent]);
-			}
-			{
-				let code = r#" == "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpEquality]);
-			}
-			{
-				let code = r#" = "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpAssign]);
-			}
-			{
-				let code = r#" != "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpInequality]);
-			}
-			{
-				let code = r#" < "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpLess]);
-			}
-			{
-				let code = r#" <= "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpLessOrEqual]);
-			}
-			{
-				let code = r#" > "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpGreater]);
-			}
-			{
-				let code = r#" >= "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpGreaterOrEqual]);
-			}
-			{
-				let code = r#" ( "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpLeftParen]);
-			}
-			{
-				let code = r#" ) "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::OpRightParen]);
-			}
-			{
-				let code = r#" ;  "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::SymSemicolon]);
-			}
-			{
-				let code = r#" ,  "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::SymComma]);
-			}
-			{
-				let code = r#" | "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::SymPipe]);
-			}
-			{
-				let code = r#" ] "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::SymRightBracket]);
-			}
-			{
-				let code = r#" [ "#;
-				let lexer = Lexer::new();
-				let results = lexer.lex(code);
-				assert_eq!(results, vec![Token::SymLeftBracket]);
-			}
+		{
+			let code = r#" < "#;
+			let lexer = Lexer::new();
+			let results = lexer.lex(code);
+			assert_eq!(results, vec![Token::Less]);
 		}
+		{
+			let code = r#" <= "#;
+			let lexer = Lexer::new();
+			let results = lexer.lex(code);
+			assert_eq!(results, vec![Token::LessOrEqual]);
+		}
+		{
+			let code = r#" > "#;
+			let lexer = Lexer::new();
+			let results = lexer.lex(code);
+			assert_eq!(results, vec![Token::Greater]);
+		}
+		{
+			let code = r#" >= "#;
+			let lexer = Lexer::new();
+			let results = lexer.lex(code);
+			assert_eq!(results, vec![Token::GreaterOrEqual]);
+		}
+		{
+			let code = r#" ( "#;
+			let lexer = Lexer::new();
+			let results = lexer.lex(code);
+			assert_eq!(results, vec![Token::LeftParen]);
+		}
+		{
+			let code = r#" ) "#;
+			let lexer = Lexer::new();
+			let results = lexer.lex(code);
+			assert_eq!(results, vec![Token::RightParen]);
+		}
+		{
+			let code = r#" ;  "#;
+			let lexer = Lexer::new();
+			let results = lexer.lex(code);
+			assert_eq!(results, vec![Token::Semicolon]);
+		}
+		{
+			let code = r#" ,  "#;
+			let lexer = Lexer::new();
+			let results = lexer.lex(code);
+			assert_eq!(results, vec![Token::Comma]);
+		}
+		{
+			let code = r#" | "#;
+			let lexer = Lexer::new();
+			let results = lexer.lex(code);
+			assert_eq!(results, vec![Token::Pipe]);
+		}
+		{
+			let code = r#" ] "#;
+			let lexer = Lexer::new();
+			let results = lexer.lex(code);
+			assert_eq!(results, vec![Token::RightBracket]);
+		}
+		{
+			let code = r#" [ "#;
+			let lexer = Lexer::new();
+			let results = lexer.lex(code);
+			assert_eq!(results, vec![Token::LeftBracket]);
+		}
+	}
+
+	#[test]
+	fn it_handles_empty_string() {
+		let code = "";
+		let lexer = Lexer::new();
+		let results = lexer.lex(code);
+
+		assert_eq!(results, vec![]);
+	}
+	#[test]
+	fn it_handles_whitespace_string() {
+		let code = " 	 ";
+		let lexer = Lexer::new();
+		let results = lexer.lex(code);
+
+		assert_eq!(results, vec![]);
 	}
 
 }
