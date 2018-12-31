bb1a9c77df5b57662e9fd86da0b508a5cdedbca1
jack-t <jackmaverick1@gmail.com>
2018-12-31 18:56:24 -0500

    Start parsing structure

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,5 +1,4 @@
 pub mod tree;
-use core::iter::Peekable;
 pub use self::tree::*;
 use super::lex::Token;
 
@@ -10,7 +9,7 @@ fn parse_tokens(mut tokens: Vec<Token>) -> Option<Box<Block>> {
 	}
 
 	let mut root = Box::new(Block{
-		statement: parse_statement(&mut tokens).unwrap(),
+		statement: parse_statement(&mut tokens).expect("Could not parse initial tokens."),
 		next: parse_statement(&mut tokens),
 	});
 
@@ -18,6 +17,44 @@ fn parse_tokens(mut tokens: Vec<Token>) -> Option<Box<Block>> {
 }
 
 fn parse_statement(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
-	let iter = tokens.iter().peekable();
-	// now, a big match to figure out what kind of statement we've got 
+		if let Some(tok) = tokens.first() {
+			match tok {
+				Token::Id(_) | Token::LeftParen => parse_expression(tokens),
+				Token::If => parse_conditional(tokens),
+				Token::While => parse_loop(tokens),
+				Token::Return => parse_return(tokens),
+				Token::Fn => parse_fn_decl(tokens),
+				Token::Type => parse_type_alias_decl(tokens),
+				Token::Contract => parse_contract(tokens),
+				Token::LeftBrace => parse_block(tokens),
+				_ => panic!("Unexpected {:?}; expected statement-initial token.", tok)
+			}
+		} else {
+			None
+		}
+}
+
+fn parse_expression(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+	None
+}
+fn parse_conditional(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+	None
+}
+fn parse_loop(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+	None
+}
+fn parse_return(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+	None
+}
+fn parse_fn_decl(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+	None
+}
+fn parse_type_alias_decl(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+	unimplemented!();
+}
+fn parse_contract(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+	unimplemented!();
+}
+fn parse_block(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+	None
 }
