e4b11ab37790b81369bae86e31b9b7dced1729f9
jack-t <jackmaverick1@gmail.com>
2018-12-31 22:01:58 -0500

    Use a deque instead of a vec in parsing

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,8 +1,9 @@
 pub mod tree;
 pub use self::tree::*;
 use super::lex::Token;
+use std::collections::VecDeque;
 
-fn parse_tokens(mut tokens: Vec<Token>) -> Option<Box<Block>> {
+fn parse_tokens(mut tokens: VecDeque<Token>) -> Option<Box<Block>> {
     let initial = parse_statement(&mut tokens);
 
     if let Some(initial) = initial {
@@ -15,8 +16,8 @@ fn parse_tokens(mut tokens: Vec<Token>) -> Option<Box<Block>> {
     }
 }
 
-fn parse_statement(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
-    if let Some(tok) = tokens.first() {
+fn parse_statement(tokens: &mut VecDeque<Token>) -> Option<Box<Statement>> {
+    if let Some(tok) = tokens.front() {
         match tok {
             Token::Id(_) | Token::LeftParen => parse_expression(tokens),
             Token::If => parse_conditional(tokens),
@@ -33,29 +34,36 @@ fn parse_statement(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
     }
 }
 
-fn parse_expression(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+fn parse_expression(tokens: &mut VecDeque<Token>) -> Option<Box<Statement>> {
     None
 }
-fn parse_conditional(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+fn parse_conditional(tokens: &mut VecDeque<Token>) -> Option<Box<Statement>> {
     None
 }
-fn parse_loop(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+fn parse_loop(tokens: &mut VecDeque<Token>) -> Option<Box<Statement>> {
     None
 }
-fn parse_return(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+fn parse_return(tokens: &mut VecDeque<Token>) -> Option<Box<Statement>> {
     None
 }
-fn parse_fn_decl(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+fn parse_fn_decl(tokens: &mut VecDeque<Token>) -> Option<Box<Statement>> {
     None
 }
-fn parse_type_alias_decl(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+fn parse_type_alias_decl(tokens: &mut VecDeque<Token>) -> Option<Box<Statement>> {
     unimplemented!();
 }
-fn parse_contract(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+fn parse_contract(tokens: &mut VecDeque<Token>) -> Option<Box<Statement>> {
     unimplemented!();
 }
-fn parse_block(tokens: &mut Vec<Token>) -> Option<Box<Statement>> {
+fn parse_block(tokens: &mut VecDeque<Token>) -> Option<Box<Statement>> {
     None
+}
+
+fn next_tok_is(tokens: &mut VecDeque<Token>, tok: Token) {
+	match tokens.pop_front() {
+		Some(ref next) if next == &tok => (),
+		next @ _ => panic!("Parse error: Expected {:?}, got {:?}", tok, next),
+	}
 }
 
 #[cfg(test)]
@@ -63,8 +71,26 @@ mod tests {
     use super::*;
     #[test]
     fn it_handles_empty_tok_stream() {
-        let toks = vec![];
+        let toks = VecDeque::new();
         let result = parse_tokens(toks);
         assert_eq!(result, None);
+    }
+
+    mod internal {
+    	use super::super::*;
+    	#[test]
+    	fn test_assertion_utility_good() {
+    		let mut toks = VecDeque::from(vec![Token::Id("abc".to_string()), Token::LeftParen]);
+    		let result = next_tok_is(&mut toks, Token::Id("abc".to_string()));
+    		assert_eq!(result, ());
+    		assert_eq!(toks, vec![Token::LeftParen]);
+    	}
+
+    	#[test]
+    	#[should_panic]
+    	fn test_assertion_utility_bad() {
+    		let mut toks = VecDeque::from(vec![Token::Id("abc".to_string()), Token::LeftParen]);
+    		let result = next_tok_is(&mut toks, Token::LeftParen);
+    	}
     }
 }
